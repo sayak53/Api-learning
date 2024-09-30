5d94e62c0ac87565e1bbd377e1ae8b56db2f36a2
sayak53 <sayakchakraborty928@gmail.com>
2024-09-30 19:22:11 +0530

    interesting to learn data fetching and displaying it in the website

--- fetchApi/fetch.js
+++ fetchApi/fetch.js
@@ -1,3 +1,18 @@
 fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+        .then(response => response.json())
+        .then(data => console.log(data))
+
+function fetchUsers(){
+     fetch('https://jsonplaceholder.typicode.com/users')
+          .then(res => res.json())
+          .then(data => displayUsers(data))
+}
+
+function displayUsers(data){
+    const ul =document.getElementById('username-container')
+    for(user of data){
+       const li = document.createElement('li')
+       li.innerText=`Name:${user.name} and lives in ${user.address.city}`
+       ul.appendChild(li)
+    }
+}
